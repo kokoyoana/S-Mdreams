49cced104c1e3ba54448ad5ada326cef0f4aa7b6
yoana <koko-yoana@hotmail.es>
2021-04-16 10:43:35 +0200

    Views articulos

--- tienda/app/urls.py
+++ tienda/app/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
    path('',Home.as_view(), name='Index'),  
    path('inicio/',Home.as_view(), name='inicio'),  
    path('categorias/',Categorias.as_view(), name='categorias'),
+   path('productos/',Articulos.as_view(), name='productos'),
+
    path('infoProducto/<int:pk>/',InfoProducto.as_view(), name='infoProducto'),
 
  
--- tienda/app/views.py
+++ tienda/app/views.py
@@ -39,17 +39,33 @@ class Categorias(ListView):
     
     def get_context_data(self,**kwargs):
         context=super(Categorias, self).get_context_data(**kwargs)
+        idSer = self.kwargs.get('pk',None)
+        context['especialidad'] = Producto.objects.filter(producto_id=idSer)
+        context['productos'] = Producto.objects.all()
+        context['nuevos']= Producto.objects.filter(nuevos = True)[:3]
+        context['algunos']= Producto.objects.filter(algunos = True)[:6]
+        context['destacados']= Producto.objects.filter(destacados = True)[:3]
+
 
         return context
 
 
-class InfoProducto(DetailView):
-    template_name = 'app/index.html'
-    model = Categoria
 
+class Articulos(TemplateView):
+    template_name = 'app/index.html'
+    context_object_name= 'productos' 
+    queryset = Producto.objects.all()
+    model = Producto
+    
     def get_context_data(self,**kwargs):
-        context=super(InfoProducto, self).get_context_data(**kwargs)
+        context=super(Articulos, self).get_context_data(**kwargs)
         idSer = self.kwargs.get('pk',None)
-        context['especialidad'] = Producto.objects.filter(servicio_id=idSer)
-        context['infoSer']= Producto.objects.get(pk = idSer)
-        context['servi'] = Producto.objects.all()
+        context['infopro'] = Producto.objects.filter(categoria_id=idSer)
+        context['categorias']= Categoria.objects.all()
+        context['productos'] = Producto.objects.all()
+        context['nuevos']= Producto.objects.filter(nuevos = True)[:3]
+        context['algunos']= Producto.objects.filter(algunos = True)[:6]
+        context['destacados']= Producto.objects.filter(destacados = True)[:3]
+
+
+        return context
